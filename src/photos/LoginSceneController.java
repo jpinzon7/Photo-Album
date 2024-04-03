f3b2bc27652f5a29fdc2f924159cc40e8790552d
maxvysh <45989000+maxvysh@users.noreply.github.com>
2024-04-03 00:50:30 -0400

    alert for empty username

--- src/photos/LoginSceneController.java
+++ src/photos/LoginSceneController.java
@@ -2,6 +2,7 @@ package photos;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -12,34 +13,36 @@ public class LoginSceneController {
     @FXML
     private TextField usernameInput;
 
-    //String username = the text in the username textfield
-
+    // String username = the text in the username textfield
 
     public void login(ActionEvent event) {
-        if (usernameInput.getText().equals("admin")) { //if the user inputs "admin" as the username, then go to AdminScene
+        String username = usernameInput.getText();
+        if (username.equals("admin")) { // if the user inputs "admin" as the username, then go to AdminScene
             Main mainInstance = new Main();
             mainInstance.switchToAdminScene();
+        } else if (username.isEmpty()) {
+            // Alert the user that a username is required
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Username Required");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter a username.");
+            alert.showAndWait();
+        } else {
+            // if the user inputs anything other than admin as the username, then go to
+            // UserScene
+            Main mainInstance = new Main();
+            mainInstance.switchToUserScene();
         }
-        else { //if the user inputs anything other than admin as the username, then go to UserScene
-                Main mainInstance = new Main();
-                mainInstance.switchToUserScene();
-            }
-        }
-
-    
-
-    //if username != admin, then go to UserScene
-
-
-
-
-
-/* 
-    public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
     }
-*/
 }
-    
+
+// if username != admin, then go to UserScene
+
+/*
+ * public void initialize() {
+ * String javaVersion = System.getProperty("java.version");
+ * String javafxVersion = System.getProperty("javafx.version");
+ * label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " +
+ * javaVersion + ".");
+ * }
+ */
