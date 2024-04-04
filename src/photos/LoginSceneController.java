b3918afdbd510daff74ac86e68b3736282ce1c05
maxvysh <45989000+maxvysh@users.noreply.github.com>
2024-04-04 00:35:29 -0400

    Better documentation

--- src/photos/LoginSceneController.java
+++ src/photos/LoginSceneController.java
@@ -1,6 +1,6 @@
 package photos;
 
-import java.util.*;
+import java.io.*;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,25 +12,29 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.Node;
-import java.io.*;
 
+
+/**
+ * Controller for the login scene.
+ * @author Jorge Pinzon, Maxim Vyshnevsky
+ */
 public class LoginSceneController {
-
     @FXML
     private Label label;
     @FXML
     private TextField usernameInput;
-    // String username = the text in the username textfield
 
+    // Runs when the login button is clicked
     public void login(ActionEvent event) {
-        String username = usernameInput.getText();
-        if (username.equals("admin")) {
+        String username = usernameInput.getText(); // Get the username from the text field
+        if (username.equals("admin")) { // If the username is admin, go to the admin scene
             try {
+                // Load the AdminScene
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("AdminScene.fxml"));
                 Parent root = loader.load();
                 Scene adminScene = new Scene(root);
 
-                // Get the UserSceneController and set the username
+                // Get the AdminSceneController and set the username
                 AdminSceneController adminSceneController = loader.getController();
                 adminSceneController.initialize(username);
 
@@ -40,14 +44,15 @@ public class LoginSceneController {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        } else if (username.isEmpty()) {
+        } else if (username.isEmpty()) { // Username cannot be empty, show an alert
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Username Required");
             alert.setHeaderText(null);
             alert.setContentText("Please enter a username.");
             alert.showAndWait();
-        } else {
+        } else { // If the username is not empty and not admin, go to the user scene
             try {
+                // Load the UserScene
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("UserScene.fxml"));
                 Parent root = loader.load();
                 Scene userScene = new Scene(root);
