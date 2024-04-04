b865e39790cea01872a4ba0d09efe6f8de054191
jpinzon7 <pinzon24j@gmail.com>
2024-04-04 13:10:06 -0400

    Improved AlbumScene, dynamically adds and removes photos

--- src/photos/AlbumSceneController.java
+++ src/photos/AlbumSceneController.java
@@ -1,5 +1,42 @@
 package photos;
 
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
+
 public class AlbumSceneController {
-    
+    @FXML
+    private GridPane gridPane;
+
+    public void addPhoto(Image image, String caption) {
+        ImageView imageView = new ImageView(image);
+        Label label = new Label(caption);
+        Button deleteButton = new Button("Delete");
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(imageView);
+        borderPane.setBottom(label);
+
+        // Create a container for the button and align it to the right
+        HBox hbox = new HBox(deleteButton);
+        hbox.setAlignment(Pos.TOP_RIGHT);
+        borderPane.setTop(hbox);
+
+        int numberOfCells = gridPane.getChildren().size();
+        int newRow = numberOfCells / gridPane.getColumnConstraints().size();
+        int newColumn = numberOfCells % gridPane.getColumnConstraints().size();
+
+        gridPane.add(borderPane, newColumn, newRow);
+    }
+
+    public void deletePhoto(BorderPane borderPane) {
+        gridPane.getChildren().remove(borderPane);
+    }
+
 }
