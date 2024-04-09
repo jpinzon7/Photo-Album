9039d3094a314d9e41b2acf4f42b115eb165ae0d
maxvysh <45989000+maxvysh@users.noreply.github.com>
2024-04-08 21:47:23 -0400

    Photos instead of Main

--- src/photos/model/Photos.java
+++ src/photos/model/Photos.java
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  * @author Maxim Vyshnevsky and Jorge Pinzon
  */
 
-public class Main extends Application {
+public class Photos extends Application {
     
     /** 
      * @param primaryStage
